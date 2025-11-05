0b87069b3eb2aadf1c84118f37154e85e9abf587
IKHIZIROV <ismailkhizirov@gmail.com>
2025-11-05 11:50:29 +0100

    labo05

--- labo05/oefn-state-filtering/src/App.tsx
+++ labo05/oefn-state-filtering/src/App.tsx
@@ -8,17 +8,45 @@ interface Student {
 }
 
 function App() {
-  const students : Student[] = [];
+  const [search, setSearch] = useState("")
 
-  const SearchStudent = () => {
-    return 
-  }
+  const studentList : Student[] = [
+    { name: "Emma Janssens", age: 20, year: 2 },
+    { name: "Lars Peeters", age: 21, year: 3 },
+    { name: "Sofie Vermeulen", age: 19, year: 1 },
+    { name: "Noah De Smet", age: 22, year: 4 },
+    { name: "Mila Van Damme", age: 20, year: 2 }
+  ]
 
   return (
     <>
-      <p>Enter the name of the student</p>
-      <input onClick={SearchStudent} type="text" />
-      
+      <div>
+        <label>
+          Search student by name:
+          <br />
+          <input value={search} onChange={(event) => setSearch(event.target.value)} type="text" />
+        </label>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studentList.filter((student) => student.name.includes(search)).map((student, i) =>
+          <tr key={i}>
+            <td>{student.name}</td>
+            <td>{student.age}</td>
+            <td>{student.year}</td>
+          </tr>
+          )}
+        </tbody>
+      </table>
+
     </>
   )
 }
--- labo05/oefn-state-shopping-list/src/components/ShoppingList.tsx
+++ labo05/oefn-state-shopping-list/src/components/ShoppingList.tsx
@@ -9,16 +9,65 @@ interface Item {
 const ShoppingList = () => {
 
     const [ShoppingList, setShoppingList] = useState<Item[]>([])
+    const [name, setName] = useState<string>("");
+    const [amount, setAmount] = useState<number>(0);
 
-    const addItem = (name: string, amount: number) => {
-        let newItem : Item = {name: name, amount: amount}
-        setShoppingList([...ShoppingList, newItem])
+    const [succesMessage, setSuccesMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const addItem = () => {
+        if (amount < 1) {
+            setErrorMessage("Amount should be higher than 0!")
+            setSuccesMessage("")
+        } else {
+            let newItem : Item = {name: name, amount: amount}
+            setShoppingList([...ShoppingList, newItem])
+            setSuccesMessage("New item added!")
+            setErrorMessage("")
+        }
+    }
+
+    const removeItem = (index: number) => {
+        let shoppingListCopy = ShoppingList.filter((_, i) => i !== index ) 
+        setShoppingList(shoppingListCopy)
+        setErrorMessage("Item removed")
+        setSuccesMessage("")
     }
 
     return(
         <>
-            <input type="text" />
-            <input type="text" />
+            {succesMessage && <div style={{color: "white", backgroundColor: "green", border: "1px solid black"}} >{succesMessage}</div>}
+            {errorMessage && <div style={{color: "white", backgroundColor: "red", border: "1px solid black"}} >{errorMessage}</div>}
+
+            <label>Name</label>
+            <input type="text" value={name} onChange={(event) => setName(event?.target.value)}/>
+
+            <label>Quantity</label>
+            <input type="number" value={amount} onChange={(event) => setAmount(parseInt(event.target.value))} />
+
+            <button onClick={addItem}>Add</button>
+            
+            <table style={{border: "solid black 1px", padding: "5px", margin: "2px"}} >
+                <thead>
+                    <tr>
+                        <th style={{border: "solid black 1px", padding: "5px", margin: "2px"}} >Name</th>
+                        <th style={{border: "solid black 1px", padding: "5px", margin: "2px"}} >Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+            {ShoppingList.map((item, index) => {
+                return(
+                <tr>
+                    <td style={{border: "solid black 1px", padding: "5px", margin: "2px"}} >{item.name}</td>
+                    <td style={{border: "solid black 1px", padding: "5px", margin: "2px"}} >{item.amount}</td>
+                    <td>
+                    <button onClick={() => {removeItem(index)}} style={{padding: "5px", margin: "2px", backgroundColor: "Red"}} >
+                        Remove</button>
+                    </td>
+                </tr>)
+            })}
+            </tbody>
+            </table>
         </>
     )
 }
